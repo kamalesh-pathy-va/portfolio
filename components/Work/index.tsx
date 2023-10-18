f0c50fb026823276cc5027af3f631f060b31bd46
kamalesh-pathy-va <asokankamalesh@gmail.com>
2023-10-18 22:55:20 +0530

    Added this project to list of works

--- components/Work/index.tsx
+++ components/Work/index.tsx
@@ -1,13 +1,24 @@
 import { BiLogoReact, BiLogoTailwindCss, BiLogoHtml5, BiLogoCss3, BiLogoJavascript, BiLogoTypescript } from 'react-icons/bi'
+import { SiNextdotjs } from 'react-icons/si'
 import spotifyClonePreview from '@/public/spotifyClonePreview.png'
 import watchCarousel from '@/public/watchCarousel.png'
 import artGalary from '@/public/artgalary.png'
+import portfolio from '@/public/PortfolioSite.png'
 import DisplayCard from './DisplayCard'
 
 const Work = () => {
   const cardData = [
     {
       key: 1,
+      title: "Portfolio Site",
+      techIcon: [<SiNextdotjs key={1} />, <BiLogoTailwindCss key={2} />, <BiLogoTypescript key={3} />],
+      techName: ["Next.js", "Tailwind CSS", "Typescript"],
+      demoURL: "https://portfolio-bay-eight-19.vercel.app/",
+      description: "My Portfolio site made with Next 13, Tailwind CSS and Typescript",
+      imagePath: portfolio,
+    },
+    {
+      key: 2,
       title: "Spotify Clone",
       techIcon: [<BiLogoReact key={1} />, <BiLogoTailwindCss key={2} />],
       techName: ["React", "Tailwind CSS"],
@@ -16,7 +27,7 @@ const Work = () => {
       imagePath: spotifyClonePreview,
     },
     {
-      key: 2,
+      key: 3,
       title: "Watch Carousel",
       techIcon: [<BiLogoHtml5 key={1} />, <BiLogoCss3 key={2} />, <BiLogoJavascript key={3} />],
       techName:["HTML", "CSS", "JavaScript"],
@@ -25,7 +36,7 @@ const Work = () => {
       imagePath: watchCarousel,
     },
     {
-      key: 3,
+      key: 4,
       title: "Art Galary",
       techIcon: [<BiLogoHtml5 key={1} />, <BiLogoCss3 key={2} />],
       techName: ["HTML", "CSS", "JavaScript"],
